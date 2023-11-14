223b43ab878edec1cac23ead3a2bcfe64a961430
yrkim <yrkimoffice@gmail.com>
2023-11-15 00:37:14 +0900

    chapter04 : 설계 품질과 트레이드 오프

---
+++ src/main/java/com/yrkim/objects/chapter04/Customer.java
@@ -0,0 +1,11 @@
+package main.java.com.yrkim.objects.chapter04;
+
+public class Customer {
+    private String name;
+    private String ide;
+
+    public Customer(String name, String ide) {
+        this.name = name;
+        this.ide = ide;
+    }
+}
---
+++ src/main/java/com/yrkim/objects/chapter04/ReservationAgency.java
@@ -0,0 +1,43 @@
+package main.java.com.yrkim.objects.chapter04;
+
+import main.java.com.yrkim.objects.chapter02.Money;
+
+public class ReservationAgency {
+    public Reservation reserve(Screening screening, Customer customer, int audienceCount) {
+        Movie movie = screening.getMovie();
+        boolean discountable = false;
+
+        for (DiscountCondition condition : movie.getDiscountConditions()) {
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                discountable = screening.getWhenScreened().getDayOfWeek().equals(condition.getDayOfWeek()) &&
+                        condition.getStartTime().isBefore(screening.getWhenScreened().toLocalTime()) &&
+                        condition.getEndTime().isAfter(screening.getWhenScreened().toLocalTime());
+            } else {
+                discountable = condition.getSequence() == screening.getSequence();
+            }
+
+            if (discountable) {
+                break;
+            }
+        }
+
+        Money fee;
+
+        if (discountable) {
+            Money discountAmount = Money.ZERO;
+            switch (movie.getMovieType()) {
+                case AMOUNT_DISCOUNT -> {
+                    discountAmount = movie.getDiscountAmount();
+                }
+                case PERCENT_DISCOUNT -> {
+                    discountAmount = movie.getFee().times(movie.getDiscountPercent());
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + movie.getMovieType());
+            }
+            fee = movie.getFee().minus(discountAmount);
+        } else {
+            fee = movie.getFee();
+        }
+        return new Reservation(customer, screening, fee, audienceCount);
+    }
+}
